d4337542801f0f607beeaffff1775845337d21c0
sofani <sofanib@gmail.com>
2019-08-12 19:47:59 -0400

    updated spring boot

---
+++ Demo_SpringBootJPA_CRUD 4/src/main/java/com/infy/DemoSpringBootJPACRUDApplication.java
@@ -0,0 +1,100 @@
+package com.infy;
+
+import java.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import com.infy.model.Customer;
+import com.infy.service.CustomerServiceImpl;
+
+@SpringBootApplication
+public class DemoSpringBootJPACRUDApplication implements CommandLineRunner {
+	@Autowired
+	CustomerServiceImpl customerService;
+
+	@Autowired
+	Environment environment;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJPACRUDApplication.class, args);
+
+	}
+
+	public void run(String... args) throws Exception {
+
+		// addCustomer();
+		 getCustomer();
+		// updateCustomer();
+		// deleteCustomer();
+	}
+
+	public void addCustomer() {
+
+		Customer customer = new Customer();
+		customer.setCustomerId(441);
+		customer.setEmailId("amit@infy.com");
+		customer.setName("Amit Kumar");
+		customer.setDateOfBirth(LocalDate.now());
+
+		try {
+			Integer id = customerService.addCustomer(customer);
+			System.out.println(environment
+					.getProperty("UserInterface.INSERT_SUCCESS") + id);
+		} catch (Exception e) {
+
+			if (e.getMessage() != null)
+				System.out
+						.println(environment.getProperty(e.getMessage(),
+								"Something went wrong. Please check log file for more details."));
+		}
+
+	}
+
+	public void getCustomer() {
+		try {
+
+			Customer customer = customerService.getCustomer(441);
+			System.out.println("Customer id : " + customer.getCustomerId());
+			System.out.println("Customer name : " + customer.getName());
+			System.out.println("Customer email : " + customer.getEmailId());
+
+		} catch (Exception e) {
+
+			if (e.getMessage() != null)
+				System.out
+						.println(environment.getProperty(e.getMessage(),
+								"Something went wrong. Please check log file for more details."));
+		}
+	}
+
+	public void deleteCustomer() {
+		try {
+			customerService.deleteCustomer(441);
+			System.out.println(environment
+					.getProperty("UserInterface.DELETE_SUCCESS"));
+		} catch (Exception e) {
+
+			if (e.getMessage() != null)
+				System.out
+						.println(environment.getProperty(e.getMessage(),
+								"Something went wrong. Please check log file for more details."));
+		}
+	}
+
+	public void updateCustomer() {
+		try {
+			customerService.updateCustomer(441, "amit_kumar@infy.com");
+			System.out.println(environment
+					.getProperty("UserInterface.UPDATE_SUCCESS"));
+		} catch (Exception e) {
+
+			if (e.getMessage() != null)
+				System.out
+						.println(environment.getProperty(e.getMessage(),
+								"Something went wrong. Please check log file for more details."));
+		}
+	}
+}
---
+++ Demo_SpringBootJPA_CRUD 4/src/main/java/com/infy/entity/CustomerEntity.java
@@ -0,0 +1,52 @@
+package com.infy.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Customer")
+public class CustomerEntity {
+
+	@Id
+	
+	private Integer customerId;
+
+	private String emailId;
+	private String name;
+	
+	private LocalDate dateOfBirth;
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+}
